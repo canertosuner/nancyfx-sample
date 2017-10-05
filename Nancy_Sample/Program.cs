258d3a657dfd5f98ebe421af41cce6411e2e3ff0
Caner Tosuner <canertosuner@gmail.com>
2017-10-05 10:49:47 +0300

    project created

---
+++ Nancy_Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace Nancy_Sample
+{
+    class Program
+    {
+        private readonly NancyHost _nancy;
+
+        public Program()
+        {
+            var uri = new Uri("http://localhost:7880");
+            var hostConfigs = new HostConfiguration { UrlReservations = { CreateAutomatically = true } };
+            _nancy = new NancyHost(uri, new DefaultNancyBootstrapper(), hostConfigs);
+        }
+
+        private void Start()
+        {
+            _nancy.Start();
+            Console.WriteLine($"Started listenig address {"http://localhost:7880"}");
+            Console.ReadKey();
+            _nancy.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Start();
+        }
+    }
+}
---
+++ Nancy_Sample/SampleModule.cs
@@ -0,0 +1,38 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+
+namespace Nancy_Sample
+{
+public class SampleModule : NancyModule
+{
+    public SampleModule()
+    {
+        Get["/"] = parameters => "Que pasa primo !";
+
+
+        Post["/sum"] = parameters =>
+        {
+            var request = this.Bind<SumRequestModel>();
+
+            return new SumResponseModel { Result = request.X + request.Y };
+        };
+
+
+        Get["/home"] = parameters =>
+        {
+            return View["View/Home.cshtml"];
+        };
+    }
+}
+
+    public class SumRequestModel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class SumResponseModel
+    {
+        public int Result { get; set; }
+    }
+}
